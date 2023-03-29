42bbb4ff3be881ff9f88e0adfb77edc366b4464b
Behnam Hajian <b.hajian@gmail.com>
2023-03-28 23:37:34 -0400

    fixes

---
+++ src/handler/profile-creation-dynamo-stream-handler.ts
@@ -0,0 +1,22 @@
+import {DynamoDB} from "aws-sdk";
+import {Env} from "../lib/env";
+import {ProfileService} from "../service/profile-service";
+const table = Env.get('PROFILE_TABLE')
+const bucket = Env.get('PROFILE_BUCKET')
+const reviewApiUrl = Env.get('REVIEW_API_URL')
+const profileService = new ProfileService({
+    table: table,
+    bucket: bucket,
+    reviewApiUrl: reviewApiUrl
+})
+
+export async function handler(event: any) {
+    for(let i = 0 ; i < event.Records.length; i++){
+        const record = event.Records[i]
+        if(record.eventName === 'INSERT'){
+            const unmarshalledProfile = DynamoDB.Converter.unmarshall(record.dynamodb.NewImage)
+            const res = await profileService.createReviewable(unmarshalledProfile)
+            console.log(res)
+        }
+    }
+}
